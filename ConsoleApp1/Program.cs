f14d17a986d5f5bb74d792e86b04bdef306559df
FRANCO <riccardo.arrigoni19@gmail.com>
2023-01-13 12:17:42 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class program
+{
+    public static void Main(string[] args)
+    {
+        int l;
+        Console.WriteLine(" Inserire granezza array ");
+        l = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[l];
+        Console.WriteLine(" Inserire valore nell'array ");
+    }
+
+    public static void Funzione1(string[] args)
+    {
+
+    }
+
+    public static void Funzione2(string[] args)
+    {
+
+    }
+
+    public static void Funzione3(string[] args)
+    {
+
+    }
+
+    //cancellazione di un elemento dall' array
+    public static void Funzione4(string[] args)
+    {
+
+    }
+}
